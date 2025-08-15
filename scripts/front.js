f4b55cfa9e39f72804922ed99632bbefbbd96945
Thomas Newman <tnewman057@gmail.com>
2025-08-14 21:46:54 -0400

    Pages (10 members per page)

--- scripts/front.js
+++ scripts/front.js
@@ -1,6 +1,8 @@
+const pageData = [];
+
 document.addEventListener('DOMContentLoaded', () => {
-  const memberList = document.querySelector('.member-list');
   const countSpan = document.getElementById('member-count');
+  var currentPage = 1;
 
   // Populate the member list
   fetch('data/data.csv')
@@ -8,23 +10,43 @@ document.addEventListener('DOMContentLoaded', () => {
   .then(data => {
     const rows = data.split('\n').slice(1);
     rows.forEach(row => {
-      const cols = row.split(',');
-      if (cols.length === 5) {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
-          <td><a href="${cols[0]}"><img src="${cols[0]}/favicon.ico"></a></td>
-          <td><a href="${cols[0]}" target="_blank" rel="noopener noreferrer">${cols[0].replace('https://', '')}</a></td>
-          <td>${cols[1]}</td>
-          <td>${cols[2]}</td>
-          <td>${cols[3]}</td>
-          <td>${cols[4]}</td>
-        `;
-        memberList.appendChild(tr);
-        
-      }
+      pageData.push(row.split(','));
     });
+    updateMemberList(currentPage, pageData);
     countSpan.textContent = rows.length;
   });
   
-  
+  const arrows = document.querySelectorAll('.arrow');
+  arrows.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+      if (arrow.classList.contains('left') && currentPage > 1) {
+        currentPage = currentPage - 1;
+      } else if (arrow.classList.contains('right') && currentPage < Math.ceil(pageData.length / 10)) {
+        currentPage = currentPage + 1;
+      }
+      updateMemberList(currentPage, pageData);
+    });
+  });
 });
+
+function updateMemberList(currentPage, data) {
+  const memberList = document.querySelector('.member-list');
+  memberList.replaceChildren();
+  const start = (currentPage - 1) * 10;
+  const end = start + 10;
+  const pageData = data.slice(start, end);
+  pageData.forEach(row => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td><a href="${row[0]}"><img src="${row[5]}"></a></td>
+      <td><a href="${row[0]}" target="_blank" rel="noopener noreferrer">${row[0].replace('https://', '')}</a></td>
+      <td>${row[1]}</td>
+      <td>${row[2]}</td>
+      <td>${row[3]}</td>
+      <td>${row[4]}</td>
+    `;
+    memberList.appendChild(tr);
+  });
+  const pageNumber = document.querySelector('.page-number');
+  pageNumber.textContent = `${currentPage}`;
+}
